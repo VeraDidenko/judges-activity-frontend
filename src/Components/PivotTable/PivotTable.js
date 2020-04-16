ba33d919f77a59e5ed0b18094b81b6436bb9a542
VeraDidenko <verochka1998@gmail.com>
2020-04-16 12:34:49 +0300

    status

--- src/Components/PivotTable/PivotTable.js
+++ src/Components/PivotTable/PivotTable.js
@@ -9,55 +9,212 @@ function PivotTable(props) {
     const ref = React.useRef();    
 
     let reportActivity = {
-      dataSource: {
-        type: "csv",
-        filename: "http://localhost:8080/data-activity"
-      },
-      slice: {
-
-      },
-      options: {
-          grid: {
-              type: "flat",
-              showTotals: "off",
-              showGrandTotals: "off", 
-          }
-      },
+    "dataSource": {
+        "type": "csv",
+        "filename": "http://localhost:8080/data-activity"
+    },
+    "slice": {
+        "rows": [
+            {
+                "uniqueName": "Judge"
+            }
+        ],
+        "columns": [
+            {
+                "uniqueName": "[Measures]"
+            }
+        ],
+        "measures": [
+            {
+                "uniqueName": "Total Activity",
+                "aggregation": "sum"
+            },
+            {
+                "uniqueName": "Score",
+                "aggregation": "sum"
+            }
+        ],
+        "sorting": {
+            "column": {
+                "type": "desc",
+                "tuple": [],
+                "measure": {
+                    "uniqueName": "Total Activity",
+                    "aggregation": "sum"
+                }
+            }
+        }
+    },
+    "options": {
+        "grid": {
+            "showTotals": "off",
+            "showGrandTotals": "off"
+        }
+    },
     };
 
     let reportMonth = {
-      dataSource: {
-        type: "csv",
-        filename: "http://localhost:8080/data-month"
-      },
-      slice: {
-
-      },
-      options: {
-          grid: {
-              type: "flat",
-              showTotals: "off",
-              showGrandTotals: "off", 
-          }
-      },
+                        "dataSource": {
+                            "type": "csv",
+                            "filename": "http://localhost:8080/data-month"
+                        },
+                        "slice": {
+                            "rows": [
+                                {
+                                    "uniqueName": "Judge",
+                                    "filter": {
+                                        "measure": {
+                                            "uniqueName": "Month Activity",
+                                            "aggregation": "sum"
+                                        },
+                                        "query": {
+                                            "top": 10
+                                        }
+                                    }
+                                }
+                            ],
+                            "columns": [
+                                {
+                                    "uniqueName": "Month",
+                                    "filter": {
+                                        "members": [
+                                            "month.[2]"
+                                        ]
+                                    }
+                                },
+                                {
+                                    "uniqueName": "[Measures]"
+                                }
+                            ],
+                            "measures": [
+                                {
+                                    "uniqueName": "Month Activity",
+                                    "aggregation": "sum"
+                                }
+                            ],
+                            "sorting": {
+                                "column": {
+                                    "type": "desc",
+                                    "tuple": [],
+                                    "measure": {
+                                        "uniqueName": "Month Activity",
+                                        "aggregation": "sum"
+                                    }
+                                }
+                            }
+                        },
+                        "options": {
+                            "grid": {
+                                "showTotals": "off",
+                                "showGrandTotals": "off"
+                            }
+                        },
+                        "conditions": [
+                            {
+                                "formula": "#value > 400",
+                                "measure": "Month Activity",
+                                "format": {
+                                    "backgroundColor": "#F44336",
+                                    "color": "#000000",
+                                    "fontFamily": "Arial",
+                                    "fontSize": "12px"
+                                }
+                            }
+                        ],
+                      
     };
 
     let reportStatus = {
-      dataSource: {
-        type: "csv",
-        filename: "http://localhost:8080/data-status"
-      },
-      slice: {
-
-      },
-      options: {
-          grid: {
-              type: "flat",
-              showTotals: "off",
-              showGrandTotals: "off", 
-          }
-      },
-    };
+    "dataSource": {
+        "type": "csv",
+        "filename": "http://localhost:8080/data-status"
+    },
+    "slice": {
+        "rows": [
+            {
+                "uniqueName": "Judge"
+            }
+        ],
+        "columns": [
+            {
+                "uniqueName": "[Measures]"
+            }
+        ],
+        "measures": [
+            {
+                "uniqueName": "Rejected",
+                "aggregation": "sum"
+            },
+            {
+                "uniqueName": "Accepted",
+                "aggregation": "sum"
+            }
+        ],
+        "sorting": {
+            "column": {
+                "type": "desc",
+                "tuple": [],
+                "measure": {
+                    "uniqueName": "Rejected",
+                    "aggregation": "sum"
+                }
+            }
+        },
+        "flatSort": [
+            {
+                "uniqueName": "Rejected",
+                "sort": "desc"
+            },
+            {
+                "uniqueName": "Accepted",
+                "sort": "desc"
+            }
+        ]
+    },
+    "options": {
+        "grid": {
+            "type": "flat",
+            "showTotals": "off",
+            "showGrandTotals": "off"
+        },
+        "chart": {
+            "activeMeasure": {
+                "uniqueName": {}
+            }
+        }
+    },
+    "conditions": [
+        {
+            "formula": "#value = 0",
+            "measure": "Rejected",
+            "format": {
+                "backgroundColor": "#8BC34A",
+                "color": "#000000",
+                "fontFamily": "Arial",
+                "fontSize": "12px"
+            }
+        },
+        {
+            "formula": "#value = 0",
+            "measure": "Accepted",
+            "format": {
+                "backgroundColor": "#F44336",
+                "color": "#000000",
+                "fontFamily": "Arial",
+                "fontSize": "12px"
+            }
+        },
+        {
+            "formula": "#value > 100",
+            "measure": "Rejected",
+            "format": {
+                "backgroundColor": "#FF9800",
+                "color": "#000000",
+                "fontFamily": "Arial",
+                "fontSize": "12px"
+            }
+        }
+    ]};
 
     const setTheme = (cssUrl) => {
         var prevThemeTags = getPrevTheme();
@@ -199,17 +356,73 @@ function PivotTable(props) {
                     width="100%"
                     height="100%"
                     report={{
-                        dataSource: {
-                          type: "csv",
-                          filename: "http://localhost:8080/data-status"
-                      },
-                        options: {
-                            grid: {
-                                type: "flat",
-                                showTotals: "off",
-                                showGrandTotals: "off", 
+                        "dataSource": {
+                            "type": "csv",
+                            "filename": "http://localhost:8080/data-month"
+                        },
+                        "slice": {
+                            "rows": [
+                                {
+                                    "uniqueName": "Judge",
+                                    "filter": {
+                                        "measure": {
+                                            "uniqueName": "Month Activity",
+                                            "aggregation": "sum"
+                                        },
+                                        "query": {
+                                            "top": 10
+                                        }
+                                    }
+                                }
+                            ],
+                            "columns": [
+                                {
+                                    "uniqueName": "Month",
+                                    "filter": {
+                                        "members": [
+                                            "month.[2]"
+                                        ]
+                                    }
+                                },
+                                {
+                                    "uniqueName": "[Measures]"
+                                }
+                            ],
+                            "measures": [
+                                {
+                                    "uniqueName": "Month Activity",
+                                    "aggregation": "sum"
+                                }
+                            ],
+                            "sorting": {
+                                "column": {
+                                    "type": "desc",
+                                    "tuple": [],
+                                    "measure": {
+                                        "uniqueName": "Month Activity",
+                                        "aggregation": "sum"
+                                    }
+                                }
                             }
                         },
+                        "options": {
+                            "grid": {
+                                "showTotals": "off",
+                                "showGrandTotals": "off"
+                            }
+                        },
+                        "conditions": [
+                            {
+                                "formula": "#value > 400",
+                                "measure": "Month Activity",
+                                "format": {
+                                    "backgroundColor": "#F44336",
+                                    "color": "#000000",
+                                    "fontFamily": "Arial",
+                                    "fontSize": "12px"
+                                }
+                            }
+                        ],
                       }}
                     beforetoolbarcreated={customizeToolbar}
                 />
